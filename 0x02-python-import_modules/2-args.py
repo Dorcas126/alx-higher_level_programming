d574964991d12686a527a1257a6332619ce1dec6
Uwimbabazi Dorcas <uwimbabazidorcas65@gmail.com>
2022-08-25 04:11:39 -0700

    two commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
      print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
                                   "." if l == 1 else ":"))
      for i, s in enumerate(argv):
-     if i > 0:
-     print("{:d}: {:s}".format(i, s))
+         if i > 0:
+            print("{:d}: {:s}".format(i, s))
