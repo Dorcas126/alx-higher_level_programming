6580dc3c70e1d795dc3d0a59830527cab05194a3
Uwimbabazi Dorcas <uwimbabazidorcas65@gmail.com>
2022-11-01 08:49:00 -0700

    seven

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
--- Displays the number of records with id = 89 in the table first_table.
-SELECT COUNT(*)
-FROM `first_table`
-WHERE `id` = 89;
+-- Inserts a new row in a table
+-- Query to insert a new row in the table first_table
+INSERT INTO first_table (id, name) VALUES (89, "Best School")
