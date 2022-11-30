d696718174fbd4d826eb4e9c20f70329cb82f797
Dorcas126 <103927814+Dorcas126@users.noreply.github.com>
2022-11-30 12:08:43 +0200

    Delete 8-esrever.js

--- 8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const esrever = (list) => {
-  const reversedArray = [];
-  list.forEach((element) => reversedArray.unshift(element));
-  return (reversedArray);
-};
-
-module.exports = { esrever };
