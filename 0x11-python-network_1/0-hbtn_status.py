a99af92200b71064ce501486f3f9f708be6e56b3
Dorcas@126 <uwimbabazidorcas65@gmail.com>
2022-12-19 14:30:40 +0000

    commits

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-
 """Fetches https://alx-intranet.hbtn.io/status"""
-
 import urllib.request
 
 
-
-
-
 if __name__ == "__main__":
-
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-
     with urllib.request.urlopen(request) as response:
-
         body = response.read()
-
         print("Body response:")
-
         print("\t- type: {}".format(type(body)))
-
         print("\t- content: {}".format(body))
